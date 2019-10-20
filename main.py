39b13a657553c186b2fcac4769a50d770010554e
Miles Budden <miles@budden.net>
2019-10-21 00:09:08 +0100

    New files

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ from pprint import pprint
 import requests
 from colorthief import ColorThief
 from PIL import Image
+from tqdm import tqdm
+
+
+key:str;
+with open("key", "r") as fp:
+    key = fp.read()
 
 r = requests.get("https://ws.audioscrobbler.com/2.0/",
                  {
                      "method": "user.gettopalbums",
                      "user": "pbexe",
-                     "api_key": "***REMOVED***",
+                     "api_key": key,
                      "format": "json",
                      "period": "1month",
-                     "limit": 1000
+                     "limit": 104
                  }).json()
-
+print("Made request")
 albums = []
 for album in r["topalbums"]["album"]:
     albums.append(album["image"][3]["#text"])
 
 album_images = []
-for image in albums:
+for image in tqdm(albums):
     try:
         path = io.BytesIO(urllib.request.urlopen(image).read())
         rgb = ColorThief(path).get_color()
@@ -45,8 +51,8 @@ for i in range(0,91,10):
 
 
 def create_collage(width, height, listofimages):
-    cols = 33
-    rows = 33
+    cols = 10
+    rows = 10
     thumbnail_width = width//cols
     thumbnail_height = height//rows
     size = thumbnail_width, thumbnail_height
@@ -76,4 +82,38 @@ def create_collage(width, height, listofimages):
 
     new_im.save("Collage.jpg")
 
-create_collage(9000, 9000, albums_twice_sorted)
+
+def create_collage_colours(width, height, listofimages):
+    cols = 10
+    rows = 10
+    thumbnail_width = width//cols
+    thumbnail_height = height//rows
+    size = thumbnail_width, thumbnail_height
+    new_im = Image.new('RGB', (width, height))
+    ims = []
+    for p in listofimages:
+        try:
+            im = p[0]
+            im.thumbnail(size)
+            ims.append(im)
+        except ValueError:
+            print("ERROR: MISSING URL!!")
+    i = 0
+    x = 0
+    y = 0
+    for col in range(cols):
+        for row in range(rows):
+            print(i, x, y)
+            try:
+                new_im.paste(ims[i], (x, y))
+            except IndexError:
+                pass
+            i += 1
+            y += thumbnail_height
+        x += thumbnail_width
+        y = 0
+
+    new_im.save("CollageColours.jpg")
+
+create_collage(1740, 1740, albums_twice_sorted)
+create_collage_colours(1740, 1740, albums_twice_sorted)
